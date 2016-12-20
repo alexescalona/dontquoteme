a2e15963baec5d027c01d1f19b7e131dc79e7bf8
Mike Matz <mike@pixor.net>
2016-12-20 14:42:07 -0600

    First attempt at multiple players

--- quote.py
+++ quote.py
@@ -9,12 +9,12 @@ from fuzzywuzzy import process
 
 
 NUM_ROUNDS = 3
-
+MAX_PLAYERS = 5
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+logger = logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
 def get_new_quote():
@@ -34,14 +34,50 @@ def check_response(provided, expected):
     return fuzz.ratio(provided.lower(), expected.lower())>=80
 
 
+def current_player():
+    return session.attributes.get('player', 1)
+
+
+def current_round():
+    return session.attributes.get('round', 1)
+
+
+def number_of_players():
+    return session.attributes.get('players', 1)
+
+
+def score_for_player(player_num):
+    score_key = "score_%d" % player_num
+    return session.attributes.get(score_key, 0)
+
+
+def set_score_for_player(player_num, score):
+    score_key = "score_%d" % player_num
+    session.attributes[score_key] = score
+
+
+def get_scores():
+    return [(n+1, score_for_player(n+1)) for n in range(number_of_players)]
+
+
+def get_sorted_scores():
+    def getkey(item):
+        return item[1]
+    return sorted(get_scores(), getkey)
+
+
+def current_players_score():
+    return score_for_player(current_player())
+
+
 def increment_score():
-    score = session.attributes.get('score', 0)
+    player_num = current_player()
+    score = score_for_player(player_num)
     score += 1
-    session.attributes['score'] = score
+    set_score_for_player(player_num, score)
 
 
-def get_points_text():
-    score = session.attributes.get('score', 0)
+def get_points_text(score):
     if score == 1:
         return "{0} point".format(score)
     elif score == 0:
@@ -50,23 +86,50 @@ def get_points_text():
         return "{0} points".format(score)
 
 
+def get_scores_text(scores=None):
+    text = ''
+    if scores is None:
+        scores = get_sorted_scores()
+    for (player_num, score) in scores:
+        text += "{0} has {0}".format(
+            get_player_text(player_num),
+            get_points_text(score),
+        )
+    return text
+
+
+def get_player_text(player_num):
+    return "Player {0}".format(player_num)
+
+
 def is_game_over():
     return session.attributes['round'] > NUM_ROUNDS
 
 
 def increment_round():
-    round_num = session.attributes.get('round', 1)
-    round_num += 1
+    player_num = current_player()
+    num_players = number_of_players()
+    round_num = current_round()
+
+    if player_num < num_players:
+        player_num += 1
+    else:
+        player_num = 1
+        round_num += 1
+
     session.attributes['round'] = round_num
+    session.attributes['player'] = player_num
 
 
 def setup_round():
     round_num = session.attributes.get('round', 1)
+    player_num = session.attributes.get('player', 1)
     quote_json = get_new_quote()
     round_msg = render_template(
         'round',
         quote=quote_json['quote'],
         round=round_num,
+        player_text=get_player_text(player_num),
     )
     session.attributes['quote_json'] = quote_json
     session.attributes['round_num'] = round_num
@@ -79,13 +142,31 @@ def new_game():
     return question(welcome_msg)
 
 
-@ask.intent("YesIntent")
-def next_round():
-    msg = setup_round()
+@ask.intent("NumberOfPlayersIntent", convert={'players': int})
+def number_of_players(players):
+    logger.debug("Number of players: %s" % players)
+    players_int = int(players)
+    if players_int < 1:
+        players_int = 1
+    elif players_int >= MAX_PLAYERS:
+        players_int = MAX_PLAYERS
+    session.attributes['players'] = players_int
+    round_message = setup_round()
+    start_message = render_template(
+        'game_start',
+        rounds=NUM_ROUNDS,
+    )
+    msg = "%s %s" % (start_message, round_message)
     return question(msg)
 
+#
+# @ask.intent("YesIntent")
+# def next_round():
+#     msg = setup_round()
+#     return question(msg)
 
-@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
+
+@ask.intent("AnswerIntent")
 def answer(author):
     quote_json = session.attributes.get('quote_json', None)
     if quote_json is None:
@@ -102,9 +183,13 @@ def answer(author):
     increment_round()
     if is_game_over():
         # Show game over message and ask if they want to play again?
+        sorted_scores = get_sorted_scores()
+        winner = sorted_scores[0][0]
+        winning_player_text = get_player_text(winner)
         game_over_message = render_template(
             'game_over',
-            points_text=get_points_text()
+            all_scores_text=get_scores_text(),
+            winning_player_text=winning_player_text,
         )
         msg = "%s %s" % (win_lose_message, game_over_message)
         return statement(msg)
