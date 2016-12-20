f5aa91f43436931824853a55b4b62ec8254ac8e1
Mike Matz <mike@pixor.net>
2016-12-20 12:45:01 -0600

    Clean up spacing and stuff

--- quote.py
+++ quote.py
@@ -1,24 +1,31 @@
-
 import logging
 import requests
 import json
-
 from random import randint
-
 from flask import Flask, render_template
-
 from flask_ask import Ask, statement, question, session
 
 
 app = Flask(__name__)
-
 ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 
-@ask.launch
+def get_new_quote():
+    response = requests.post(
+        "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
+        headers={
+            "X-Mashape-Key": "qxw2noHOjgmshc3avMkLGu49Jcvrp186kqjjsnvnLClFDEYPGG",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json"
+        }
+    )
+    quote_json = json.loads(response.text)
+    return quote_json
 
+
+@ask.launch
 def new_game():
 
     welcome_msg = render_template('welcome')
@@ -27,43 +34,26 @@ def new_game():
 
 
 @ask.intent("YesIntent")
-
 def next_round():
-  response = requests.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies",
-  headers={
-    "X-Mashape-Key": "qxw2noHOjgmshc3avMkLGu49Jcvrp186kqjjsnvnLClFDEYPGG",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Accept": "application/json"
-  }
-  )
-  quote_json = json.loads(response.text)
+    quote_json = get_new_quote()
+    round_msg = render_template('round', quote=quote_json['quote'],
+                                round=session.attributes['round'])
 
-  round_msg = render_template('round', quote=quote_json['quote'], round=session.attributes['round'])
-
-  session.attributes['quote_json']=quote_json
-  return question(round_msg)
-
-
+    session.attributes['quote_json'] = quote_json
+    return question(round_msg)
 
 
 @ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
-
 def answer(author):
-
-    
     quote_json = session.attributes['quote_json']
     if author == quote_json['author']:
-
         msg = render_template('win')
-
     else:
-
         msg = render_template('lose', author=quote_json['author'])
-
     return question(msg)
 
-@ask.intent("ContinueIntent")
 
+@ask.intent("ContinueIntent")
 def continue_round():   
     session.attributes['round'] = session.attributes['round'] + 1
     if session.attributes['round'] < 3:
@@ -73,10 +63,7 @@ def continue_round():
         return statement(msg)
 
 if __name__ == '__main__':
-
     app.run(debug=True)
-
-
 
 
 
